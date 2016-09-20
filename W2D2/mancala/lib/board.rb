60cdf8e2ec23a341659d925de240428768d0592f
Todd Nestor <todd.nestor@gmail.com>
2016-09-20 09:57:46 -0700

    start getting mancala board class working

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -2,20 +2,74 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14)
+    place_stones
+    @cup_owners = {}
+    set_cup_owners(name1,name2)
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |_,i|
+      @cups[i] = Array.new(4) {:stone} unless is_store_cup?(i)
+      @cups[i] = Array.new if is_store_cup?(i)
+    end
+  end
+
+  def set_cup_owners(name1,name2)
+    @cup_owners[name1] = 6
+    @cup_owners[name2] = 13
+  end
+
+  def is_store_cup?(i)
+    [6,13].include?(i)
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" unless on_board?(start_pos)
+  end
+
+  def on_board?(start_pos)
+    (1..14).cover?(start_pos)
+  end
+
+  def other_players_cup?(name, cup)
+    is_store_cup?(cup) && @cup_owners[name] != cup
   end
 
   def make_move(start_pos, current_player_name)
+    pieces = []
+    cup = @cups[start_pos]
+    pieces << cup.pop until cup.empty?
+
+    cup = start_pos
+
+    pieces.length.times do |i|
+      cup = next_cup(cup, current_player_name)
+      @cups[cup] << pieces.pop
+    end
+
+    render
+    next_turn(cup, current_player_name)
   end
 
-  def next_turn(ending_cup_idx)
-    # helper method to determine what #make_move returns
+  def next_cup(cup, name)
+    if is_store_cup?(cup+1) && other_players_cup?(name, cup+1)
+      next_index = cup + 2
+    else
+      next_index = cup + 1
+    end
+
+    next_index = 0 unless on_board?(next_index + 1)
+    next_index
+  end
+
+  def next_turn(ending_cup_idx, name)
+    store_cup = @cup_owners[name]
+    if @cups[ending_cup_idx].empty?
+      return :switch
+    elsif ((store_cup-6)..store_cup).cover?(ending_cup_idx)
+      return ending_cup_idx
+    end
   end
 
   def render
--- W2D2/mancala/lib/mancala.rb
+++ W2D2/mancala/lib/mancala.rb
@@ -62,3 +62,8 @@ class Mancala
     puts " 1   2   3   4   5   6"
   end
 end
+
+if __FILE__ == $PROGRAM_NAME
+  mancala = Mancala.new("Todd","Bob")
+  mancala.play
+end
