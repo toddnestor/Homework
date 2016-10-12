bd9ba71202223242b09cd350b2e8b74d55e30265
Todd Nestor <todd.nestor@gmail.com>
2016-10-12 08:36:08 -0700

    elephants can march now

--- W5D3/phase-2.js
+++ W5D3/phase-2.js
@@ -19,3 +19,39 @@ function Elephant(name, height, tricks) {
   this.height = height;
   this.tricks = tricks;
 }
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} goes phrrrrrr!!!!`);
+}
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function() {
+  index = Math.round(Math.random() * (this.tricks.length - 1) )
+
+  console.log(`${this.name} is ${this.tricks[index]}`);
+}
+
+var ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+var charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+var kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+var micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+ellie.grow()
+ellie.trumpet();
+ellie.play();
+ellie.play();
+
+var herd = [ellie, charlie, kate, micah];
+
+function elephantHelper(elephant) {
+  console.log(`${elephant.name} is marching by!`)
+}
+
+herd.forEach(elephantHelper)
